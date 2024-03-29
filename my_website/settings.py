beb969ec89b078a92d37053bd4338192abb0bc33
chagab <gabriel.chatelain@protonmail.com>
2024-03-28 17:40:54 -0700

    add AWS S3 storage settings to settings.py

--- my_website/settings.py
+++ my_website/settings.py
@@ -132,3 +132,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 django_on_heroku.settings(locals())
+
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", 'dev default value')
+AWS_SECRET_ACCESS_KEY = os.environ.get(
+    "AWS_SECRET_ACCESS_KEY", 'dev default value')
+AWS_STORAGE_BUCKET_NAME = 'gabrielchatelainwebsite'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'us-east-2'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERIFY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
